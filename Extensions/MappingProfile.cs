b3be227b9546a8311b7d6babf5a60afe06c7f0ac
tmaxcoded <tmaxcoded@outlook.com>
2021-06-20 08:37:38 +0100

    identity pilotig

---
+++ Extensions/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using IdentityByExamples.Models.USERS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityByExamples.Extensions
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserRegistrationModel, User>()
+                .ForMember(u => u.UserName, opt => opt.MapFrom(x => x.Email));
+        }
+    }
+}
---
+++ Migrations/20210620045817_insertIdentityRole.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityByExamples.Migrations
+{
+    public partial class insertIdentityRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "86576b9f-e1d7-4c22-8304-271bca3cb792", "e7eb5634-1635-40a4-b33e-c9a7bb822446", "Visitor", "VISITOR" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "1c5cd610-50d7-4fb1-9478-2beef9623722", "67622ab4-e956-46c2-bd6f-0128b3851e2d", "Administrator", "ADMINISTRATOR" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1c5cd610-50d7-4fb1-9478-2beef9623722");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "86576b9f-e1d7-4c22-8304-271bca3cb792");
+        }
+    }
+}
